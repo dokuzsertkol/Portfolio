44df57587a00ca088f2b11dd47e7e4654924ed4c
dokuzsertkol <120343813+dokuzsertkol@users.noreply.github.com>
2025-11-16 00:23:08 +0300

    Updates

---
+++ _next/static/chunks/9778c1f016405a81.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,11233,t=>{"use strict";var e=t.i(43476);function i(){return(0,e.jsx)("main",{children:(0,e.jsx)("h1",{children:"bu benim test sayfam."})})}t.s(["default",()=>i])}]);
