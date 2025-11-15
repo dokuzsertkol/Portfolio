c69a21138837c407d11660c709030ce171e9c281
dokuzsertkol <120343813+dokuzsertkol@users.noreply.github.com>
2025-11-15 20:05:39 +0300

    Updates

---
+++ _next/static/chunks/8155d7c9b868ace1.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,75472,e=>{"use strict";var t=e.i(43476);function i(){return(0,t.jsx)("main",{children:(0,t.jsx)("h1",{children:"bu benim projects sayfam."})})}e.s(["default",()=>i])}]);
